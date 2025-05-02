1eb4383243871269b4eefb833ffbddc6bec933a5
jylim <jylim@12cm.co.kr>
2025-05-02 15:26:03 +0900

    chore: 불필요한 내용 정리

--- src/main/java/com/yalmung/mini/commerce/MiniCommerceApplication.java
+++ src/main/java/com/yalmung/mini/commerce/MiniCommerceApplication.java
@@ -9,15 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class MiniCommerceApplication {
 
     public static void main(String[] args) {
-
-        System.out.println("=== args ===");
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-
-
         SpringApplication.run(MiniCommerceApplication.class, args);
-        System.out.print("시작 성공!!!!!!!!!!");
     }
 
 }
